8e964b4078c4e3c5179b8a78dc0e538e74c01525
SarahQW <108081862+SarahQW@users.noreply.github.com>
2024-01-24 23:33:08 +0300

    finished Home page style

--- x-react-project/src/pages/Profile.jsx
+++ x-react-project/src/pages/Profile.jsx
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import Trending from "../components/Trending";
-import Posted from "../components/Posted";
-
-function Home() {
+function Profile() {
   return (
     <>
       <section className="grid grid-cols-[25%_minmax(200px,_1.5fr)_1fr]">
         <div className="sticky top-0 h-min">
           <NavBar />
         </div>
+        {/* Start Profile page */}
         <div className=" border border-[#e7e7e8]">
           <div
-            className="sticky top-0 border border-[#E7E7E8] flex justify-center items-center p-1 cursor-pointer hover:bg-[#ebebebe1]"
+            className="sticky top-0 border border-[#E7E7E8] flex justify-center items-start flex-col p-1 cursor-pointer hover:bg-[#ebebebe1]"
             style={{
               background: "rgba(225,225,225, 0.0)",
               backdropFilter: "blur(20px)",
             }}
           >
-            <p className="font-bold  p-3 after:block after:absolute after:h-1 after:w-16 after:bg-[#1A8CD8] after:bottom-1">
-              For you
-            </p>
+            <p className="font-bold text-xl ps-5"> Sarah🩷</p>
+
+            <p className=" ps-5 text-xs text-[#c9c9c9e1]">125 post</p>
           </div>
           <div>
-            <Posted />
+            <div>
+              <img src="" alt="profile background image" />
+            </div>
+            <div>
+              <img src="" alt="personal image" />
+            </div>
           </div>
         </div>
         <div className="sticky top-0 h-max">
@@ -34,4 +38,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Profile;
